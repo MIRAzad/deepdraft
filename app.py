dacd3dad3034df3a296aba30f3c5dfe7a05d836f
Mir Azad <49590276+MIRAzad@users.noreply.github.com>
2024-01-29 13:19:17 +0530

    removed title color to black-

--- app.py
+++ app.py
@@ -26,8 +26,7 @@ st.set_page_config(
 
 # Set the title with improved styling
 # st.markdown("<h1 style='text-align: left; color: lightblue; font-weight: bold;'>DEEPDRAFT</h1>", unsafe_allow_html=True)
-st.title("DEEPDRAFT")
-
+st.title(" DEEPDRAFT ")
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
